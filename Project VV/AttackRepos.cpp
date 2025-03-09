5abe841648892b67f04a6c21d77ba7191b870d2a
Didenko Darya <142428557+hiretsuno@users.noreply.github.com>
2025-03-09 20:38:24 +0300

    Версия 0.2.7
    Добавлены атаки в Attack Repos

--- Project VV/AttackRepos.cpp
+++ Project VV/AttackRepos.cpp
@@ -3,7 +3,7 @@
 AttackEffect DefaultEffectFormulaTarget(struct AttackEffect Attack, std::array<int, 6>& minEmotions, std::array<int, 6>& Emotions) {
 
 	Attack.Stress += 3 * (Emotions[0]- minEmotions[0]); //страх
-
+	
 	Attack.Stress += 4 * (Emotions[1] - minEmotions[1]);//злость
 
 	Attack.Stress += 2 * (Emotions[2] - minEmotions[2]);//презрение
@@ -47,13 +47,110 @@ const std::vector<AttackData>& AttackRepositoryPlayer::GetAttacks()
 				DefaultEffectFormulaTarget // сюда имя формулы через которую будут проходить параметры
 			},
 			{
-				"xs",
-				{ 1, 1, 2, 0, 0, 0 },
-				{ 20, -4, -2 },
-				{ 20, -4, -2 },
+				"pants",
+				{ 1, 0, 0, 0, 0, 0 }, // 1 страх
+				{ 5, 0, 10 },
+				{ 15, 0, 0 },
 				DefaultEffectFormulaMe,
 				DefaultEffectFormulaTarget
-			}
+			},
+			{
+				"punch",
+				{ 0, 1, 0, 0, 0, 0 }, // 1 злость
+				{ 0, -10, -5 },
+				{ 25, 0, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"delete",
+				{ 0, 1, 0, 0, 0, 0 }, // 1 злость
+				{ 0, -10, -10},
+				{ 25, -5, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"myphic",
+				{ 0, 0, 1, 0, 0, 0 }, // 1 презрение
+				{ 0, 0, 0},
+				{ 10, -20, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"smile",
+				{ 0, 0, 0, 0, 1, 0 }, // 1 умротворенность
+				{ -15, 0, 10},
+				{ 0, 0, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"carryon",
+				{ 0, 0, 0, 1, 0, 0 }, // 1 радость
+				{ -15, 10, 0},
+				{ 0, 0, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"NPC",
+				{ 0, 0, 0, 0, 0, 1 }, // 1 вдохновение
+				{ -15, 0, 0},
+				{ 10, 0, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"norespect",
+				{ 1, 0, 1, 0, 0, 0}, // 1 страх 1 през
+				{ 0, 0, 5},
+				{ 15, -15, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"mountains",
+				{ 0, 1, 0, 0, 0, 1}, // 1 злось 1 вдох
+				{ -5, -10, 0},
+				{ 30, 0, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"miserable",
+				{ 0, 0, 1, 0, 0, 1}, // 1 през 1 вдох
+				{ -5, 5, 0},
+				{ 20, -10, 0 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"nature",
+				{ 1, 0, 1, 0, 0, 0}, // 1 през 1 злость
+				{ 5, -5, 0},
+				{ 10, -10, -10 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"truth",
+				{ 0, 0, 1, 0, 2, 0}, // 2 умир 1 през
+				{ 0, 10, 15},
+				{ 0, -5, -15 },
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+			{
+				"science",
+				{ 2, 0, 0, 0, 0, 1}, // 2 страх 1 вдох
+				{ -10, 10, 5},
+				{ 5, -15, 0},
+				DefaultEffectFormulaMe,
+				DefaultEffectFormulaTarget
+			},
+
 	};
 	return attacks;
 }
